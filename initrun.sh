a05fbeb1d59713c2305231941e5f8917337f5c6e
Petr Zvoníček <zvonicek@gmail.com>
2016-03-29 00:57:23 +0200

    Question configuration

--- initrun.sh
+++ initrun.sh
@@ -1,3 +1,6 @@
 python run.py initdb;
 python load_questions.py csv/closeended.csv closeended;
-python run.py run;
+python load_questions.py csv/numeric.csv numeric;
+python load_questions.py csv/scale.csv scale;
+python load_questions.py csv/sort.csv sort;
+# python run.py run;
